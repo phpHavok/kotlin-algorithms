69f7e0d9c470d79eafbb91466e5a35f7d0b61767
Jacob Chappell <jacob.chappell@daveramsey.com>
2022-12-30 14:11:27 -0600

    Re-initialize before each benchmark.

--- src/main/kotlin/com/github/phphavok/kotlinalgorithms/algorithms/fibonacci/Benchmark.kt
+++ src/main/kotlin/com/github/phphavok/kotlinalgorithms/algorithms/fibonacci/Benchmark.kt
@@ -2,22 +2,25 @@ package com.github.phphavok.kotlinalgorithms.algorithms.fibonacci
 
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
 private const val MAX_TIME_MS = 1000
 
 fun main() {
-    val implementations: Array<IFibonacci> = arrayOf(
-        RecursiveFibonacci(),
-        MemoizedFibonacci()
+    val implementations: Array<KClass<out IFibonacci>> = arrayOf(
+        RecursiveFibonacci::class,
+        MemoizedFibonacci::class
     )
     println("Benchmarking ${implementations.size} fibonacci implementations.")
     println("Time limit is $MAX_TIME_MS ms per run.")
     for (implementation in implementations) {
         println("Current implementation: ${implementation::class.simpleName}")
         for (i in 0U..UInt.MAX_VALUE) {
+            val instance = implementation.createInstance()
             var result: BigInteger
             val timeElapsed = measureTimeMillis {
-                result = implementation.fibonacci(i)
+                result = instance.fibonacci(i)
             }
             println("fib($i) = $result (calculated in $timeElapsed ms)")
 
